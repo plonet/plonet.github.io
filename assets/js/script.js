4c16277b9c11568eec2451d69f8dea73fae2a5d7
plonet <149794532+plonet@users.noreply.github.com>
2023-11-05 15:30:43 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,58 +3,54 @@
 
 
 /**
- * addEvent on element
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
-
-
-
-/**
  * navbar toggle
  */
 
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const navbarToggler = document.querySelector("[data-nav-toggler]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
 
-const toggleNav = function () {
-  navbar.classList.toggle("active");
-  navbarToggler.classList.toggle("active");
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 }
 
-addEventOnElem(navbarToggler, "click", toggleNav);
+/**
+ * toggle navbar & overlay when click any navbar-link
+ */
 
-const closeNav = function () {
-  navbar.classList.remove("active");
-  navbarToggler.classList.remove("active");
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 }
 
-addEventOnElem(navbarLinks, "click", closeNav);
+
 
 
 
 /**
- * header active
+ * header & go-top-btn active
+ * when window scroll down to 400px
  */
 
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
+  if (window.scrollY >= 400) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
+    goTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+    goTopBtn.classList.remove("active");
   }
 });
